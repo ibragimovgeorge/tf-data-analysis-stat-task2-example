9409f9b3df3029a58724b47f7df08639a794e4cd
ibragimovgeorge <103066850+ibragimovgeorge@users.noreply.github.com>
2023-04-08 00:34:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,12 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     s = np.std(x/47)
     
-    chi2_left = chi2.ppf(alpha/2, n-2)
-    chi2_right = chi2.ppf(1 - alpha/2, n-2)
+    chi2_left = chi2.ppf(p/2, n-2)
+    chi2_right = chi2.ppf(1 - p/2, n-2)
     sigma_left = np.sqrt((n-2) * s**2 / chi2_right)
     sigma_right = np.sqrt((n-2) * s**2 / chi2_left)
-    
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     return sigma_left, sigma_right
