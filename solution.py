49ea7456009484ab53636d574ca20efc05cb6090
ibragimovgeorge <103066850+ibragimovgeorge@users.noreply.github.com>
2023-04-08 00:28:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,8 +8,14 @@ chat_id = 201321241 # Ваш chat ID, не меняйте название пе�
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    s = np.std(x)
-    chi2_left = chi2.ppf(p/2, n-2)
-    chi2_right = chi2.ppf(1 - p/2, n-2)
-    return np.sqrt((n-2) * s**2 / chi2_right / 47), \
-           np.sqrt((n-2) * s**2 / chi2_left / 47)
+    s = np.std(x/47)
+    
+    chi2_left = chi2.ppf(alpha/2, n-2)
+    chi2_right = chi2.ppf(1 - alpha/2, n-2)
+    sigma_left = np.sqrt((n-2) * s**2 / chi2_right)
+    sigma_right = np.sqrt((n-2) * s**2 / chi2_left)
+    
+    alpha = 1 - p
+    loc = x.mean()
+    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    return sigma_left, sigma_right
